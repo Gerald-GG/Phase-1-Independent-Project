d74a8572864e64a5848449da7b1c8718a27d364b
Gerald Gicharu <gicharugerald@gmail.com>
2023-10-12 13:19:28 +0300

    Event listeners

--- index.js
+++ index.js
@@ -1,51 +1,79 @@
-document.addEventListener(`DOMContentLoaded`, () => {
-    const url ="https://gis.ducks.org/datasets/du-university-chapters/api";
-    const dataDiv = document.getElementsById("data");
-    const refreshButton = document.getElementById("refresh-button");
-    const searchInput = doument.getElementById("search-input");
+document.addEventListener('DOMContentLoaded', () => {
+    const url = 'https://services2.arcgis.com/5I7u4SJE1vUr79JC/arcgis/rest/services/UniversityChapters_Public/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json';
+    const dataDiv = document.getElementById('data-list');
+    const listElement = document.getElementById('list');
+    const refreshButton = document.getElementById('refresh-button');
+    const searchInput = document.getElementById('search-input');
+    const searchForm = document.getElementById('search-form');
+    const dataDetails = document.getElementById('data-details');
 
     // Event listener for the refresh button
-    refreshButton.addEventListener("click", () => {
-        freshData(url);
+    refreshButton.addEventListener('click', () => {
+        fetchData(url);
     });
 
     // Event listener for Search input
-    searchInput.addEventListener("input", () => {
-        // filtering data based on the search input
+    searchInput.addEventListener('input', () => {
         const searchTerm = searchInput.value.toLowerCase();
-        filterData(searchTerm)
+        filterData(searchTerm);
     });
 
     // Event listener for the form
-    document.getElementById("search-form").addEventListener("submit", (event) => {
+    searchForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const searchTerm = searchInput.value.toLowerCase();
         filterData(searchTerm);
     });
 
-    // function to fetch and display the data
-    function fetchData(url){
+    // Function to fetch and display the data
+    function fetchData(url) {
         fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error('HTTP error! Status: ${response.status}');
-            }
-            return response.json();
-        })
-        .then((data) => {
-            dataDiv.innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch((error) => {
-            console.error("Fetch error:",error);
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                displayList(data.features);
+            })
+            .catch((error) => {
+                console.error('Fetch error:', error);
+            });
+    }
+
+    // Display data as a list
+    function displayList(features) {
+        listElement.innerHTML = '';
+
+        features.forEach((feature) => {
+            const attributes = feature.attributes;
+            const listItem = document.createElement('li');
+            listItem.textContent = attributes.University_Chapter;
+            listItem.addEventListener('click', () => displayDetails(attributes));
+            listElement.appendChild(listItem);
         });
     }
 
-
-    // Filter data and display data based on search term
-    function filterData(searchTerm) {
-
+    // Display data details
+    function displayDetails(attributes) {
+        dataDetails.innerHTML = JSON.stringify(attributes, null, 2);
     }
-    
+
+    // Filter data based on search term
+    function filterData(searchTerm) {
+        const listItems = listElement.querySelectorAll('li');
+
+        listItems.forEach((item) => {
+            const text = item.textContent.toLowerCase();
+            if (text.includes(searchTerm)) {
+                item.style.display = 'list-item';
+            } else {
+                item.style.display = 'none';
+            }
+        });
+    }
+
     // Initial data fetch whenever the page loads
     fetchData(url);
 });
